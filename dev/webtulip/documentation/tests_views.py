ecaafa41452df4cef2e0feddda928aa66f532754
Ben Jackson <ben.d.jackson@outlook.com>
2017-03-19 23:47:37 +0000

    Slight work on tests

--- dev/webtulip/documentation/tests_views.py
+++ dev/webtulip/documentation/tests_views.py
@@ -5,26 +5,26 @@ from django.core.urlresolvers import reverse
 from tulip_wrapper.models import Network
 import json
 
-client = Client()
 
 class ViewsTest(TestCase):
     def setUp(self):
+        self.client = Client()
         self.testNetwork = Network.objects.create(network_name="sas_30_test", network_file="../tulip_wrapper/tests/sas_30.tlp", network_type="TLP");
 
     def test_home(self):
-        response = client.get(reverse('home'), {})
+        response = self.client.get(reverse('home'), {})
         self.assertEqual(200, response.status_code, 'Wrong status code returned for home')
 
     def test_more_info(self):
-        response = client.get(reverse('moreInfo'), {})
+        response = self.client.get(reverse('moreInfo'), {})
         self.assertEqual(200, response.status_code, 'Wrong status code returned for moreInfo')
 
     def test_delete(self):
-        response = client.get(reverse('delete'), {})
+        response = self.client.get(reverse('delete'), {})
         self.assertEqual(200, response.status_code, 'Wrong status code returned for delete')
 
     def test_upload(self):
-        response = client.post(reverse('upload'), {})
+        response = self.client.post(reverse('upload'), {})
         # TO DO
 
     def tearDown(self):
--- dev/webtulip/tulip_wrapper/tests_views.py
+++ dev/webtulip/tulip_wrapper/tests_views.py
@@ -10,14 +10,13 @@ from .tlp_json_converter import TlpJsonConverter
 
 import json
 
-client = Client()
-
 class ViewsTest(TestCase):
     def setUp(self):
+        self.client = Client()
         self.testNetwork = Network.objects.create(network_name="sas_30_test", network_file="../tulip_wrapper/tests/sas_30.tlp", network_type="TLP");
 
     def test_base(self):
-        response = client.get(reverse('loadGraph'), {'network_name': 'sas_30_test'})
+        response = self.client.get(reverse('loadGraph'), {'network_name': 'sas_30_test'})
         dict_response = json.loads(response.content.decode('utf-8'))
         self.assertEqual(True, dict_response["success"], 'View returned not successful for base')
         self.assertEqual(True, "nodes" in dict_response["data"], 'Nodes not in data for base')
@@ -27,7 +26,7 @@ class ViewsTest(TestCase):
         self.assertEqual(0, len(dict_response["data"]["numDeletedEdge"]), 'numDeletedEdge should be empty for base')
 
     def test_prune(self):
-        response = client.get(reverse('loadGraph'), {'network_name': 'sas_30_test', 'toPrune': 'true'})
+        response = self.client.get(reverse('loadGraph'), {'network_name': 'sas_30_test', 'toPrune': 'true'})
         dict_response = json.loads(response.content.decode('utf-8'))
         self.assertEqual(True, dict_response["success"], 'View returned not successful for pruning')
         self.assertEqual(True, "nodes" in dict_response["data"], 'Nodes not in data for pruning')
@@ -37,7 +36,7 @@ class ViewsTest(TestCase):
         self.assertEqual(0, len(dict_response["data"]["numDeletedEdge"]), 'numDeletedEdge should be empty for pruning')
 
     def test_edge(self):
-        response = client.get(reverse('loadGraph'), {'network_name': 'sas_30_test', 'toEdgeBundle': 'true'})
+        response = self.client.get(reverse('loadGraph'), {'network_name': 'sas_30_test', 'toEdgeBundle': 'true'})
         dict_response = json.loads(response.content.decode('utf-8'))
         self.assertEqual(True, dict_response["success"], 'View returned not successful for edge bundling')
         self.assertEqual(True, "nodes" in dict_response["data"], 'Nodes not in data for edge bundling')
@@ -47,7 +46,7 @@ class ViewsTest(TestCase):
         self.assertEqual(3, len(dict_response["data"]["numDeletedEdge"]), 'numDeletedEdge should have 3 elements for edge bundling')
 
     def test_prune_and_edge(self):
-        response = client.get(reverse('loadGraph'), {'network_name': 'sas_30_test', 'toEdgeBundle': 'true', 'toPrune': 'true'})
+        response = self.client.get(reverse('loadGraph'), {'network_name': 'sas_30_test', 'toEdgeBundle': 'true', 'toPrune': 'true'})
         dict_response = json.loads(response.content.decode('utf-8'))
         self.assertEqual(True, dict_response["success"], 'View returned not successful for pruning and edge bundling')
         self.assertEqual(True, "nodes" in dict_response["data"], 'Nodes not in data for pruning and edge bundling')
@@ -57,7 +56,7 @@ class ViewsTest(TestCase):
         self.assertEqual(1, len(dict_response["data"]["numDeletedEdge"]), 'numDeletedEdge should have 1 elements for pruning and edge bundling')
 
     def test_delete(self):
-        response = client.post(reverse('deleteGraph'), {'network_name': 'sas_30_test'})
+        response = self.client.post(reverse('deleteGraph'), {'network_name': 'sas_30_test'})
         # WHY FAILING?
         self.assertEqual(200, response.status_code, 'Bad HTTP Response')
         dict_response = json.loads(response.content.decode('utf-8'))
